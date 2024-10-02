e90914ebc6966a398d4c2dc5978a890b15127f45
Chaitya W <chaityaw@Chaityas-MacBook-Air.local>
2024-10-02 17:49:15 +0530

    Change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,6 @@ import java.util.zip.InflaterInputStream;
 public class Main {
   public static void main(String[] args) throws IOException {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
-
     // Uncomment this block to pass the first stage
     //
      final String command = args[0];
